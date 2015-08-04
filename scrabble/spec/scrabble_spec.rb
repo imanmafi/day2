a667387ccc750cd0cb0319ae38aeb26e484a0b78
Guest User <student@epicodus.com>
2015-08-04 15:10:50 -0700

    returns error if not single letter (ex. double letter or symbol

--- scrabble/spec/scrabble_spec.rb
+++ scrabble/spec/scrabble_spec.rb
@@ -5,4 +5,6 @@ describe("Hash#score") do
   it("returns a scrabble score for a letter") do
     expect(("A").score()).to(eq(1))
   end
+  it("returns error if not single error input or special character/number")
+    expect(("1").score()).to(eq("error."))
 end
