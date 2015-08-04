664da1d126422f836975ba5491aeb8b0fd8f45a5
Guest User <student@epicodus.com>
2015-08-04 11:48:43 -0700

    returns sleep in if weekend, or work if weekday

--- lib/day2.rb
+++ lib/day2.rb
@@ -1,7 +1,5 @@
  class Time
    define_method(:sleepin)  do
-     #day_of_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-   #  t = Time.now()
      the_date = self.wday()
 
      if the_date == 1 || the_date == 2 || the_date == 3 || the_date == 4 || the_date == 5
